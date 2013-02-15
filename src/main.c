64182c69288f7981885006895ec2eec7ff431ced
Riley Alethea Butler <ryla@loquimity.org>
2013-02-14 20:20:24 -0500

    Blinky interrupt test

--- src/main.c
+++ src/main.c
@@ -10,10 +10,28 @@
 #include <avr/interrupt.h>
 
 int main(void) {
-    /* Setup */
+    /* Set PD0 to input */
+    DDRD &= ~_BV(DDD0);
+    /* Enable pin change interrupt 2 (PCINT2) */
+    PCICR |= _BV(PCIE2);
+    /* Set PCINT2 to be triggered by PCINT16 on PD0 */
+    PCMSK2 |= _BV(PCINT16);
 
-    while (-1) {
-        /* Loop */
+    /* Set PB0 to output */
+    DDRB |= _BV(DDB0);
+    /* Turn PB0 on */
+    PORTB |= _BV(PORTD0);
 
-    }
+    /* Ok, start listening for interrupts */
+    sei();
+
+    /* Loop */
+    while (-1) { }
 }
+
+/* Service routine for the hall latch */
+ISR(PCINT2_vect) {
+    /* Toggle PB0 */
+    PORTB = ~PORTB;
+}
+
