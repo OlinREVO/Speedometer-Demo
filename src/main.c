a576d02b9b4028e8e8c362996b5f3bf10931a40d
Riley Alethea Butler <ryla@loquimity.org>
2013-02-14 19:27:17 -0500

    Skeleton code in main.c

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/* Speedometer Demo
+ * ================
+ *
+ * A simple test of the LED speed display and wheel speed sensor. 
+ *
+ * Olin REVO, 2013
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int main(void) {
+    /* Setup */
+
+    while (-1) {
+        /* Loop */
+
+    }
+}
