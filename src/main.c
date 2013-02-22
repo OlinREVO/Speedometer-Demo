f039e393c75abc836127f3cc2e8beb9f5a9caf7e
Alex Adkins <adkins.alexandra@gmail.com>
2013-02-21 20:33:25 -0500

    Updating main.c to account for addition of LED code

--- src/main.c
+++ src/main.c
@@ -11,11 +11,13 @@
 #include <avr/interrupt.h>
 #include <math.h>
 #include "sevensegment.h"
+#include <util/delay.h>
 
 #define SIZE 2
 #define CIRCUM .3175 * M_PI
 #define TIMESCALAR 1024
 #define F_IO 1000000
+#define F_CPU 1000000
 /* m/s to mph */
 #define UNITSCALAR 2.23694
 
@@ -33,28 +35,35 @@ int main(void) {
     i = 0;
 
     /* Set PD0 to input */
-    DDRD &= ~(_BV(DDD0));
+    //DDRD &= ~(_BV(DDD0));
     /* Enable pin change interrupt 2 (PCINT2) */
-    PCICR |= _BV(PCIE2);
+    //PCICR |= _BV(PCIE2);
     /* Set PCINT2 to be triggered by PCINT16 (on PD0) */
-    PCMSK2 |= _BV(PCINT16);
+    //PCMSK2 |= _BV(PCINT16);
 
     /* Set PORTB and PORTC, [0-6] to output */
     DDRB |= ~(_BV(DDB7));
     DDRC |= ~(_BV(DDC7));
+    DDRD = 0xff;
 
     /* Set Timer/Counter1 on with prescaler at clk_io/1024 */
-    TCCR1B |= (_BV(CS12) | _BV(CS10));
+    //TCCR1B |= (_BV(CS12) | _BV(CS10));
 
     /* All set up, start listening for interrupts */
-    sei();
+    //sei();
+    int val = 0;
+    int ledval = val * 2/5;
 
     /* Loop */
     while (-1) {
-        uint16_t test;
-        dt = (Times[i] + 0.0) * TIMESCALAR / F_IO;
-        velocity = (SIZE - 1.0)*CIRCUM/(dt ) * UNITSCALAR;
-        ssDisplay(lround(velocity), &PORTC, &PORTB);
+        //dt = (Times[i] + 0.0) * TIMESCALAR / F_IO;
+        //velocity = (SIZE - 1.0) * CIRCUM/ dt * UNITSCALAR;
+        //ssDisplay(lround(velocity), &PORTC, &PORTB);
+        ssDisplay(val, &PORTC, &PORTB);
+        PORTD = ~(0xff >> ledval);
+        //_delay_ms(500);
+        //val = (val + 1) % 22;
+        //ledval = val * 2/5;
     }
 }
 
