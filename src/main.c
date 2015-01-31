fcd849b7b1416d52f29c97d8518da0612dbe1b39
ETumang <emily.tumang@gmail.com>
2015-01-31 14:20:13 -0500

    Cleaned up code

--- src/main.c
+++ src/main.c
@@ -20,35 +20,23 @@
 
 #define maxDataLength 8
 #define SIZE 5
-#define CIRCUM .3175 * M_PI
+#define CIRCUM .3175 * M_PI//meters
 #define TIMESCALAR 1024.0
 #define F_IO 1000000
-/* m/s to mph */
-#define UNITSCALAR 2.23694
-#define DT TIMESCALAR/F_IO
+#define DT F_IO/TIMESCALAR
 
 volatile int i = 0;
 volatile float dt = 1;
 float time = 0;
-float Times[SIZE];
+float times[SIZE];
 float velocity = 10;
 
 int shunt_res= 5000;
 
-int NODE_HOME = NODE_halleffect;
-int NODE_TARGET_1 = NODE_watchdog;
-int NODE_TARGET_2 = NODE_speedometer;
+int size = sizeof(uint8_t)/sizeof(char);
 
 /* Service routine for the hall latch */
 ISR(INT1_vect) {
-    UART_putString("INTERRUPT\n");
-    /* Grab value from TC1 and reset it */
-
-        int size = sizeof(uint8_t)/sizeof(char);
-        char output[size];
-        sprintf(output,"%d",TCNT1);
-        UART_putString("\ntime\n");
-        UART_putString(output);
 
     times[i] = TCNT1;
     i = (i+1)%SIZE;
@@ -57,65 +45,45 @@ ISR(INT1_vect) {
 
 
 int main(void) {
-    int size = sizeof(int)/sizeof(char);
+
+    uint8_t msg[1];
+    initUART();
+
+    int j = 0;
+    int time = 0;
     char output[size];
-    uint8_t can[1];
     //Initialize Times to all zeros 
     for (i = 0; i < SIZE; i++) {
-      Times[i] = 0;
+      times[i] = 0;
     }
     i = 0;
  
-     /*Enable pin change interrupt 1(PCINT2) 
-    /*PCICR |= _BV(PCIE0);*/
-
-     /*Set PCImake flashNT2 to be triggered by PCINT15 (on PC7) */
-    /*PCMSK1 |= _BV(PCINT0);*/
-
-    //Turn on ADC
-    //ADCSRA |= _BV(ADEN);
-    //Enable internal reference voltage
-    //ADCSRB &= _BV(AREFEN);
-    //Set internal reference voltage as AVcc
-    //ADMUX |= _BV(REFS0);
+    //Enable pin change interrupt 2(PCINT2) 
+    PCICR |= _BV(PCIE0);
 
     // Set Timer/Counter1 on with prescaler at clk_io/1024 //for Hall latch
     TCCR1B |= (_BV(CS12) | _BV(CS10));
 
-    initCAN(NODE_HOME);
+    initCAN(NODE_speedometer);
     sei(); // enable global interrupts  
     EICRA |= _BV(ISC10);
     EIMSK |= _BV(INT1);
     /* All set up, start listening for interrupts */
 
-    initUART();
     /* Loop */
     while (1) {
         _delay_ms(100);
 
-        int j = 0;
-        int time = 0;
         for(j = 0; j < SIZE; j++){
             time = time+times[j];
         }
 
-        uint8_t v = (int)(CIRCUM*SIZE/time);
+        uint8_t v = (int)(CIRCUM*SIZE/(time/F_IO/TIMESCALAR));
+        v= time/DT;
+        msg[0] = v;
 
-        sprintf(output,"%d",v);
-        UART_putString("\nvelocity\n");
-        UART_putString(output);
-        can[0] = v;
-        sendCANmsg(NODE_TARGET_2, MSG_speed,can,1);
+        sendCANmsg(NODE_speedometer, MSG_speed,(uint8_t*)msg,1);
 
-        /*ADCSRA |=  _BV(ADSC);
-            while(bit_is_set(ADCSRA, ADSC));
-
-            //ADC is a macro to combine ADCL and ADCH
-            uint8_t voltage = ADC;
-            uint8_t v[2];
-            v[0]= voltage;
-
-        sendCANmsg(NODE_TARGET_1,MSG_data_other, v, 2);*/
     }
 }
 
